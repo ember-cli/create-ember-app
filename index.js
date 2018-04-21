ede4ac589979d3128872ff42bd14d8524bfd0889
Chris Manson <chris@stonecircle.io>
2018-04-21 15:09:25 +0100

    implementing conditional --yarn arg

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 var execa = require('execa');
 
 var args = ['new'];
+
+if (process.env._ && !process.env._.match(/(npx|npm)$/)) {
+  args.push('--yarn');
+}
+
 for (var i = 2; i < process.argv.length; i++) {
   args.push(process.argv[i]);
 }
